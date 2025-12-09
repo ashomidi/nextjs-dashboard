8cf8f7b7c458afce6f22ba78290f98ff0aa37dbf
ashomidi <omidiashkan71@gmail.com>
2025-12-09 04:08:31 -0500

    Server actions added

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,10 +13,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
+      params.delete("page");
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
